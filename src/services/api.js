e79fddee527132e49aaa51b60b3740607e8b1d7c
gidaltibn <gidaltibn@outlook.com>
2024-09-25 11:31:23 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL:'http://10.201.30.192:8080/', //ip do tce
-   // baseURL:'http://192.168.3.8:8080/', //ip da casa de mamãe
-    //baseURL:'http://192.168.3.20:8080/', //ip de casa
-    //baseURL:'http://192.168.249.145:8080/', //ip da undb sempre muda
     baseURL:'https://laplage-api.herokuapp.com/', //url heroku
 });
 
